6a14916c93c0a7198fdf2a93457d578987ee6541
Ankur Tyagi <ankur2004tyagi@gmail.com>
2025-08-22 18:23:19 +0530

    Fixing the Download CV button by using it as row anchor tag

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -45,17 +45,20 @@ const Hero1 = () => {
 								position="right"
 							/>
 						</a>
-						<a href="/Ankur_Resume.pdf"
-						   target="_blank"
-						   rel="noopener noreferrer"
-						   download
-						>
-							<MagicButton
-								title="Download CV"
-								icon={<FaDownload />}
-								position="right"
-							/>
+						<a
+							href="/Ankur_Resume.pdf"
+							target="_blank"
+							rel="noopener noreferrer"
+							download
+							className="relative inline-flex h-12 w-full overflow-hidden rounded-lg p-[1px] focus:outline-none md:w-60 md:mt-10"
+							>
+							<span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)] pointer-events-none" />
+							<span className="gap-2 z-10 inline-flex h-full w-full items-center justify-center rounded-lg bg-slate-950 px-7 text-sm font-medium text-white backdrop-blur-3xl">
+								Download CV
+								<FaDownload />
+							</span>
 						</a>
+
 					</div>
 				</div>
 				<Spline
